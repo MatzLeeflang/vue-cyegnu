e914b98ffaece69858b4c5ed9ac92eb3763767ab
MatzLeeflang <matz@leeflang-it.nl>
2022-04-28 13:21:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-cyegnu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-cyegnu)
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <main>
+      <div>
+        <input type="text" class="search-bar" placeholder="Search..."/>
+
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      api_key: 'bdf57302f8fd5e16ab7a98781b54f1ff'
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+</style>
+
